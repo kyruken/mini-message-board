bc49be94f1822bc9d51ccb9ad13d2741cf1f31f5
Avery Kyle Arenas <kyruken@gmail.com>
2022-10-17 11:15:01 -0700

    create interface for messages

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date()
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date()
+  },
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', messages: messages});
 });
+
+router.get('/new', function(req, res) {
+  res.render('form')
+})
 
 module.exports = router;
